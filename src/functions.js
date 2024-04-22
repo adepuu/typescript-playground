0ada4c622218e97fefb1ca0aa5028094a9b7a47c
Christofer Bryan <christoferbryan45@gmail.com>
2024-04-22 20:02:41 +0700

    feat(exercise): added functions.ts file and function to calculate area of rectangle

---
+++ src/functions.js
@@ -0,0 +1,6 @@
+function rectangle(length, width) {
+    return length * width;
+}
+var a = 5;
+var b = 4;
+console.log(rectangle(a, b));
---
+++ src/functions.ts
@@ -0,0 +1,7 @@
+function rectangle(length:number, width:number){
+    return length*width;
+}
+
+let a : number = 5;
+let b: number = 4;
+
